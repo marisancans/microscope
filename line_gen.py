dcbf34d1c6fd03e9146d97987b3c5ebe34859a35
maris <marisancans@gmail.com>
2020-04-15 11:31:06 +0300

    refactoring

--- line_gen.py
+++ line_gen.py
@@ -26,81 +26,85 @@ class Rotor:
         y += yc 
         return x, y
 
+@dataclass
+class RangeC: #C - container
+    start: float
+    end: float
 
-# params
-s = 1000
-r = 50
+    def __call__(self):
+        return self.start, self.end
+
+@dataclass
+class SquiggleGen:
+    size: float = 1000
+    n_rotors: int = 25
+    n_tail: int = 100
+    rotor_radius: RangeC = RangeC(10, 100)
+    rotor_coef: RangeC = RangeC(0.01, 0.001)
+    debug: bool = True
+
+    def gen_rots(self):
+        rots = []
+        for i in range(self.n_rotors):
+            radius = random.uniform(*self.rotor_radius())
+            coef = random.uniform(*self.rotor_coef())
+            reverse = random.getrandbits(1)
+            rot = Rotor(radius, coef, reverse) 
+            rots.append(rot)
+        return rots
+
+    def gen_imgs(self, n_samples, pos):
+        blank = np.zeros((self.size, self.size, 3))
+        rand_pos = lambda a, b : (random.randint(a, b), random.randint(a, b))
+        
+        for _ in range(n_samples):
+            rots = self.gen_rots()
+            coords = []
+
+            a = rand_pos(0, self.size)
+            b = rand_pos(0, self.size)
+            # a = (0, 0)
+            # b = (s, s)
+            points_on_line = np.linspace(a, b, 100) # 100 samples on the line
+            points_on_line = points_on_line.tolist()
+            points_on_line = [(int(x), int(y)) for x, y in points_on_line]
+            points_on_line = [pos for x in range(1000)]
+
+            for pos in points_on_line:
+                img = blank.copy()
+                p = pos
+
+                for i, rot in enumerate(rots):
+                    x, y = rot(p)
+                    img = cv2.line(img, p, (int(x), int(y)), (1, 0, 0), 2)
+                    p = (int(x), int(y))
+                    
+                coords.append([x, y])
+
+                for prev, curr in zip(coords, coords[1:]):
+                    ax, ay = prev
+                    bx, by = curr
+                    a = (int(ax), int(ay))
+                    b = (int(bx), int(by))
+                    img = cv2.line(img, a, b, (1, 1, 1), 3)
+
+                cv2.namedWindow("img", cv2.WINDOW_NORMAL)
+                cv2.imshow("img", img)
+                cv2.waitKey(1)
+
+                while len(coords) > self.n_tail:
+                    coords.pop(0)
 
 
 
-pos = (int(s/2), int(s/2))
+pos = (500, 500)
 
 
-
-
-blank = np.zeros((s, s, 3))
-
-# rots = []
-# rot = Rotor(80, 0.00007) 
-# rots.append(rot)
-# rot = Rotor(40, 0.00018) 
-# rots.append(rot)
-# rot = Rotor(23, 0.002532) 
-# rots.append(rot)
-# rot = Rotor(18, 0.002342) 
-# rots.append(rot)
-
+sg = SquiggleGen()
+sg.gen_imgs(100, pos)
 
 # offset
-coords = []
 
-rand_pos = lambda a, b : (random.randint(a, b), random.randint(a, b))
 
-for _ in range(1000):
-    rots = []
-    r_max = 100
-    for i in range(25):
-        radius = random.uniform(10, r_max)
-        # r_max *= 0.99
-        coef = random.uniform(0.01, 0.001)
-        reverse = random.getrandbits(1)
-        rot = Rotor(radius, coef, reverse) 
-        rots.append(rot)
 
-    a = rand_pos(0, s)
-    b = rand_pos(0, s)
-    # a = (0, 0)
-    # b = (s, s)
-    points_on_line = np.linspace(a, b, 100) # 100 samples on the line
-    points_on_line = points_on_line.tolist()
-    points_on_line = [(int(x), int(y)) for x, y in points_on_line]
-    points_on_line = [pos for x in range(1000)]
 
-    for pos in points_on_line:
-        img = blank.copy()
-        p = pos
-
-        for i, rot in enumerate(rots):
-            x, y = rot(p)
-            img = cv2.line(img, p, (int(x), int(y)), (1, 0, 0), 2)
-            p = (int(x), int(y))
-            
-        coords.append([x, y])
-
-        for prev, curr in zip(coords, coords[1:]):
-            if prev == 0 or curr == 0:
-                continue
-
-            ax, ay = prev
-            bx, by = curr
-            a = (int(ax), int(ay))
-            b = (int(bx), int(by))
-            img = cv2.line(img, a, b, (1, 1, 1), 3)
-
-        cv2.namedWindow("img", cv2.WINDOW_NORMAL)
-        cv2.imshow("img", img)
-        cv2.waitKey(1)
-
-        while len(coords) > 100:
-            coords.pop(0)
-    coords.append(0)
--- main.py
+++ main.py
@@ -50,56 +50,15 @@
 
 
 
-from __future__ import absolute_import
-
-import sys
 
 import pygame
+import imgui
 import OpenGL.GL as gl
+import cv2
 
 from imgui.integrations.pygame import PygameRenderer
-import imgui
-from OpenGL.GL import *
-import cv2
-from dataclasses import dataclass
-import numpy as np
+from image_widget import ImageWidget
 
-@dataclass
-class ImageWidget:
-    img_np: np.array
-    width: int = 0
-    height: int = 0
-
-    def __post_init__(self):
-        self.texture_id = 0
-        self.loadImage(self.img_np)
-        
-
-    def redraw(self, img_np):
-        self.img_np = img_np
-        self.loadImage(self.img_np)
-        self.draw()
-
-    def draw(self):
-        imgui.image(self.texture_id, self.width, self.height)
-        print(self.texture_id)
-
-    # make sure the image is RGBA 
-    def loadImage(self, img_np):
-        if not self.width or not self.height:
-            self.width, self.height, channels = self.img_np.shape
-
-        textureData = img_np.tobytes()
-
-        if self.texture_id:
-            glDeleteTextures(1, self.texture_id)
-
-        self.texture_id = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, self.texture_id)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.width, self.height, 0, GL_RGBA,
-                    GL_UNSIGNED_BYTE, textureData)
 
 
 def main():
